2a86d12bdc1cd794dbcdf2808168a78867f95cd3
quang-kingfood <quang.ngovan@kingfoodmart.net>
2023-11-17 23:57:48 +0700

    feat: up vps and config pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps : [
+      {
+        name: "support-call-web",
+        script: "./index,js",
+        instances: "max",
+        exec_mode: "cluster",
+        watch: true,
+        max_memory_restart: "4G", 
+        env: {
+          NODE_ENV: "production"
+        },
+        log_date_format: "YYYY-MM-DD HH:mm Z",
+        error_file: "logs/err.log",
+        out_file: "logs/out.log",
+        merge_logs: true,
+      }
+    ]
+  };
+  
